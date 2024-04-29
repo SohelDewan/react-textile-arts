b703bed929c44dc8f663f513ad8a3864481c9b25
SohelDewan <sohelds241@gmail.com>
2024-04-30 01:33:54 +0600

    theme added

--- src/components/ArtCraftCategories.jsx
+++ src/components/ArtCraftCategories.jsx
@@ -1,9 +1,9 @@
 const ArtCraftCategories = () => {
   return (
-   <div>
-    <h2 className="text-5xl font-bold text-center mt-16">All Sub Categories</h2>
+   <div className="bg-[#E8DFCA]">
+    <h2 className="text-5xl font-bold text-center pt-8 mt-16">All Sub Categories</h2>
      <div className="grid lg:grid-cols-3 p-12 gap-8">
-      <div>
+      <div className='animate__animated animate__fadeInLeftBig'>
         <h3 className="text-3xl py-4">Embroidery</h3>
         <img
           className="w-96 h-72 rounded-lg"
@@ -12,7 +12,7 @@ const ArtCraftCategories = () => {
         />
         <p className="mt-3">Embroidery: This intricate craft involves decorating fabric using needle and thread. Embroiderers create beautiful patterns, motifs, and designs by stitching onto fabric.</p>
       </div>
-      <div>
+      <div className='animate__animated animate__fadeInLeftBig'>
         <h3 className="text-3xl py-4">Knitting & Crocheting</h3>
         <img
           className="w-96 h-72 rounded-lg"
@@ -21,7 +21,7 @@ const ArtCraftCategories = () => {
         />
          <p className="mt-3">Knitting & Crocheting: Both knitting and crocheting involve creating fabric from yarn. Knitting uses two needles to form interlocking loops, while crocheting uses a single hook to create loops and stitches.</p>
       </div>
-      <div>
+      <div className='animate__animated animate__fadeInLeftBig'>
         <h3 className="text-3xl py-4">Quilting</h3>
         <img
           className="w-96 h-72 rounded-lg"
@@ -30,7 +30,7 @@ const ArtCraftCategories = () => {
         />
          <p className="mt-3">Quilting: Quilting is the art of sewing together layers of fabric to create a warm, padded blanket or decorative textile. Quilters often use intricate patterns and patchwork designs..</p>
       </div>
-      <div>
+      <div className='animate__animated animate__fadeInRight'>
         <h3 className="text-3xl py-4">Beadwork</h3>
         <img
           className="w-96 h-72 rounded-lg"
@@ -39,7 +39,7 @@ const ArtCraftCategories = () => {
         />
          <p className="mt-3">Beadwork: Beadwork encompasses a wide range of techniques, including bead embroidery, loom weaving, and stringing beads. Artists create jewelry, accessories, and decorative items using colorful beads.</p>
       </div>
-      <div>
+      <div className='animate__animated animate__fadeInRight'>
         <h3 className="text-3xl py-4">Tie-Dyeing</h3>
         <img
           className="w-96 h-72 rounded-lg"
@@ -48,7 +48,7 @@ const ArtCraftCategories = () => {
         />
          <p className="mt-3">Tie-Dyeing: Tie-dyeing is a fun and vibrant technique where fabric is folded, twisted, or tied before dyeing. The result is a unique, colorful pattern. It gained popularity during the hippie movement in the 1960s.</p>
       </div>
-      <div>
+      <div className='animate__animated animate__fadeInRight'>
         <h3 className="text-3xl py-4">Macrame</h3>
         <img
           className="w-96 h-72 rounded-lg"
